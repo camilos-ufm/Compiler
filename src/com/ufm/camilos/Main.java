6566d94c98bdd577610d0567375eb96fee646955
andresryes <enriquebolanos@ufm.edu>
2020-09-01 18:40:33 -0600

    changes

--- src/com/ufm/camilos/Main.java
+++ src/com/ufm/camilos/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello world");
+        System.out.println("Hello world 2");
     }
 }
