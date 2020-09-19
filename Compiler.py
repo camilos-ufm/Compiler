b2d8fd06acd05108a43c0fad6a834319f82ffb5b
andresryes <enriquebolanos@ufm.edu>
2020-09-19 11:36:26 -0600

    cli params working

---
+++ Compiler.py
@@ -0,0 +1,11 @@
+#imports
+import sys
+
+if(len(sys.argv)==3):
+    print("file: " + sys.argv[0])
+    print("[option]: " + sys.argv[1])
+    print("<filename>: " + sys.argv[2])
+elif(len(sys.argv)==2):
+    print("mostrar ayuda")
+else:
+    print("falta param option")
