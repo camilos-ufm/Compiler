db1cd25ace045878d6a3370704e41091d564fdd3
Katherine Mazariegos <katherinemazariegos@ufm.edu>
2020-09-19 11:46:51 -0600

    Help

--- Compiler.py
+++ Compiler.py
@@ -6,6 +6,13 @@ if(len(sys.argv)==3):
     print("[option]: " + sys.argv[1])
     print("<filename>: " + sys.argv[2])
 elif(len(sys.argv)==2):
-    print("mostrar ayuda")
+    print("Help\n"
+        "-o <outname>     Escribir el output a <outname>\n"
+        "-target <stage>  <stage> es uno de los siguientes elementos: scan, parse, ast, semantic, irt, codegen\n"
+        "-opt <opt_stage> <opt_stage> es uno de: constant, algebraic\n"
+        "-debug <stage>   Debugging <stage>\n")
 else:
     print("falta param option")
+    
+    
+    
