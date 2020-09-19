38d2218b5bfac1e94d28cd3a845639a7c3496145
danisnowman <danielernesto@ufm.edu>
2020-09-19 12:41:39 -0600

    adds always run

--- Compiler.py
+++ Compiler.py
@@ -13,30 +13,31 @@ def opt(opt_stage):
 def debug(stage):
     print(stage)
 
-if (len(sys.argv) >= 4 and len(sys.argv)%2==0):
-    #fixed
-    print("file: " + sys.argv[0])
-    print("<filename>: " + sys.argv[1])
+if __name__ == "__main__":
+    if (len(sys.argv) >= 4 and len(sys.argv)%2==0):
+        #fixed
+        print("file: " + sys.argv[0])
+        print("<filename>: " + sys.argv[1])
 
-    #variable
-    for i in range(2,len(sys.argv)):
-        if(i%2==0):
-            if(sys.argv[i]=='-o'):
-                output(sys.argv[i+1])
-            elif(sys.argv[i]=='-target'):
-                target(sys.argv[i+1])
-            elif(sys.argv[i]=='-opt'):
-                opt(sys.argv[i+1])
-            elif(sys.argv[i]=='-debug'):
-                debug(sys.argv[i+1])
-            else:
-                print('invalid param')
+        #variable
+        for i in range(2,len(sys.argv)):
+            if(i%2==0):
+                if(sys.argv[i]=='-o'):
+                    output(sys.argv[i+1])
+                elif(sys.argv[i]=='-target'):
+                    target(sys.argv[i+1])
+                elif(sys.argv[i]=='-opt'):
+                    opt(sys.argv[i+1])
+                elif(sys.argv[i]=='-debug'):
+                    debug(sys.argv[i+1])
+                else:
+                    print('invalid param')
 
-elif (len(sys.argv) == 2):
-    print("Show Help\n"
-        "-o <outname>     Escribir el output a <outname>\n"
-        "-target <stage>  <stage> es uno de los siguientes elementos: scan, parse, ast, semantic, irt, codegen\n"
-        "-opt <opt_stage> <opt_stage> es uno de: constant, algebraic\n"
-        "-debug <stage>   Debugging <stage>\n")
-else:
-    print("'option' parameter missing.")
+    elif (len(sys.argv) == 2):
+        print("Show Help\n"
+            "-o <outname>     Escribir el output a <outname>\n"
+            "-target <stage>  <stage> es uno de los siguientes elementos: scan, parse, ast, semantic, irt, codegen\n"
+            "-opt <opt_stage> <opt_stage> es uno de: constant, algebraic\n"
+            "-debug <stage>   Debugging <stage>\n")
+    else:
+        print("'option' parameter missing.")
