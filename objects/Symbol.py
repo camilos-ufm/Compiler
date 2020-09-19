876702514fb0a6a9f97a24cded3216ea781eda40
danisnowman <danielernesto@ufm.edu>
2020-09-19 14:38:19 -0600

    adds lit everything

---
+++ objects/Symbol.py
@@ -0,0 +1,9 @@
+class Symbol:
+
+    def __init__(self, file_name):
+        self.type_num = type_num
+        self.name = name
+        self.regEx = regEx
+
+    def pretty_print(self)
+        print("<Type: " + self.type_num + ", Name: " + self.name+">")
