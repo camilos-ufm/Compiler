b7ca31a9ff8536c60a8c64697bf231a1d8de0b4e
danisnowman <danielernesto@ufm.edu>
2020-09-19 12:47:18 -0600

    adds global settings

--- Compiler.py
+++ Compiler.py
@@ -1,43 +1,58 @@
 # imports
 import sys
 
+FILE_NAME = "out"
+STAGE = "codegen"
+OPT_STAGE = ""
+DEBUG_STAGE = ""
+
+
 def output(outname):
-    print(outname)
+    FILE_NAME = outname
+    # print(outname)
+
 
 def target(stage):
-    print(stage)
+    # scan, parse, ast, semantic, irt, codegen
+    STAGE = stage
+    # print(stage)
+
 
 def opt(opt_stage):
-    print(opt_stage)
+    OPT_STAGE = opt_stage
+    # print(opt_stage)
+
 
 def debug(stage):
-    print(stage)
+    DEBUG_STAGE = stage
+    # print(stage)
+
 
 if __name__ == "__main__":
-    if (len(sys.argv) >= 4 and len(sys.argv)%2==0):
-        #fixed
+    if (len(sys.argv) >= 4 and len(sys.argv) % 2 == 0):
+        # fixed
         print("file: " + sys.argv[0])
         print("<filename>: " + sys.argv[1])
 
-        #variable
-        for i in range(2,len(sys.argv)):
-            if(i%2==0):
-                if(sys.argv[i]=='-o'):
+        # variable
+        for i in range(2, len(sys.argv)):
+            if(i % 2 == 0):
+                if(sys.argv[i] == '-o'):
                     output(sys.argv[i+1])
-                elif(sys.argv[i]=='-target'):
+                elif(sys.argv[i] == '-target'):
                     target(sys.argv[i+1])
-                elif(sys.argv[i]=='-opt'):
+                elif(sys.argv[i] == '-opt'):
                     opt(sys.argv[i+1])
-                elif(sys.argv[i]=='-debug'):
+                elif(sys.argv[i] == '-debug'):
                     debug(sys.argv[i+1])
                 else:
                     print('invalid param')
 
     elif (len(sys.argv) == 2):
         print("Show Help\n"
-            "-o <outname>     Escribir el output a <outname>\n"
-            "-target <stage>  <stage> es uno de los siguientes elementos: scan, parse, ast, semantic, irt, codegen\n"
-            "-opt <opt_stage> <opt_stage> es uno de: constant, algebraic\n"
-            "-debug <stage>   Debugging <stage>\n")
+              "-o <outname>     Escribir el output a <outname>\n"
+              "-target <stage>  <stage> es uno de los siguientes elementos: scan, parse, ast, semantic, irt, codegen\n"
+              "-opt <opt_stage> <opt_stage> es uno de: constant, algebraic\n"
+              "-debug <stage>   Debugging <stage>\n")
     else:
         print("'option' parameter missing.")
