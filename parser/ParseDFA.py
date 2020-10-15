397a6ed20456b589ed03fd8aed16a2853a382d7b
andresryes <enriquebolanos@ufm.edu>
2020-10-15 14:41:45 -0600

    parsing with mpv

--- parser/ParseDFA.py
+++ parser/ParseDFA.py
@@ -1,3 +1,5 @@
+import objects.Node as Node
+
 class ParseDFA:
 
     states_stack = [0]
@@ -5,17 +7,18 @@ class ParseDFA:
     tokens_stack = ['$']
 
     grammar = [
-        {'<program>': ['class', , 'Program', '{', '}', '$']},
+        {'<program>': ['class','Program', '{', '}']}
         # {'<X>': ['(', '<X>', ')']},
         # {'<X>': ['(', ')']}
     ]
 
     dfa_parse = {
-            0:{'class':['shift', 1],
+            0:{'class':['shift', 1], '<program>':['goto', 5]},
             1:{'Program':['shift', 2]},
-            2:{'{':['shift', 3],
+            2:{'{':['shift', 3]},
             3:{'}':['shift', 4]},
-            4:{'$':['accept',2 ],
+            4:{'}':['reduce', 1]},
+            5:{'$':['accept', 2]}
             # 5:{'(':['reduce', 2], ')':['reduce', 2], '$':['reduce', 2]},
     }
 
@@ -39,15 +42,18 @@ class ParseDFA:
         print(param_list)
         while index<=len(token_list):
             #print(token_list[index].symbol_type.name)
+            print(state)
             if (param_list != None):
                 if(param_list[0]=='shift'):
                     current_token = token_list[index].symbol_type.name
                     self.tokens_stack.append(current_token)
                     param_list = self.dfa_parse.get(self.states_stack[-1]).get(current_token)
                     self.states_stack.append(param_list[1])
+                    index+=1
+                    if(index<len(token_list)):
+                        current_token = token_list[index].symbol_type.name
                     param_list = self.dfa_parse.get(self.states_stack[-1]).get(current_token)
                     print("shift")
-                    index+=1
                 elif(param_list[0]=='goto'):
                     print(current_token)
                     print(param_list[1])
@@ -57,7 +63,6 @@ class ParseDFA:
                         param_list = self.dfa_parse.get(self.states_stack[-1]).get(current_token)
                     else:
                         #param_list = self.dfa_parse.get(self.states_stack[-1]).get()
-                        print("hola", self.tokens_stack[-1])
                         print(param_list)
                         self.tokens_stack.pop(-1)
                         self.states_stack.pop(-1)
@@ -65,19 +70,26 @@ class ParseDFA:
                     print('goto')
                 elif(param_list[0]=='reduce'):
                     print(param_list[1])
+                    node = Node.Node(list(self.grammar[param_list[1]-1].keys())[0], list(self.grammar[param_list[1]-1].values())[0])
                     count = len(list(self.grammar[param_list[1]-1].values())[0])
                     self.tokens_stack = self.tokens_stack[:-count]
                     self.states_stack = self.states_stack[:-(count)]
                     self.tokens_stack.append(list(self.grammar[param_list[1]-1].keys())[0])
                     current_token = self.tokens_stack[-1]
+                    print(self.states_stack)
                     param_list = self.dfa_parse.get(self.states_stack[-1]).get(current_token)
                     #self.states_stack.append(param_list[1])
+                    print("node list", node.token_list)
+                    print("node", node)
                     print("reduce")
                 elif(param_list[0]=='accept'):
                     print("accept :)")
             else:
                 print("state not defined")
-                print("Invalid token",token_list[index].symbol_type.name,"at line",token_list[index].line)
+                if(index<len(token_list)):
+                    print("Invalid token",token_list[index].symbol_type.name,"at line",token_list[index].line)
+                else:
+                    print("Invalid token",token_list[index-1].symbol_type.name,"at line",token_list[index-1].line)
                 break
             print(self.tokens_stack)
             print(self.states_stack)
