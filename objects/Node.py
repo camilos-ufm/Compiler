60b31b3cf7167fc9b32bc6e45fb0b20e071b7e7d
andresryes <enriquebolanos@ufm.edu>
2020-11-02 11:37:04 -0600

    verify terminal nodes

--- objects/Node.py
+++ objects/Node.py
@@ -22,7 +22,8 @@ class Node:
         if(len(self.node_list)!=0 and self.type_node!=''):
             inner_counter = 0
             for node1 in self.node_list:
-                if(len(node1.node_list)==0 and node1.object_node.symbol_type.name=="id"):
+                print(type(node1.object_node))
+                if(len(node1.node_list)==0 and str(type(node1.object_node)) == "<class 'objects.Token.Token'>" and node1.object_node.symbol_type.name=="id"):
                     if(inner_counter!=0 and (self.node_list[inner_counter-1].object_node.symbol_type.name == 'type' or self.node_list[inner_counter-1].object_node.symbol_type.name == 'void')):
                         is_unique = True
                         for symbol_verify in symbol_table[-1]:
@@ -42,18 +43,18 @@ class Node:
                         if not(is_decl):
                             error_list.append("Semantic error, Uniqueness check: Var not declared in line "+str(node1.object_node.line))
                 
-                if(len(node1.node_list)==0 and node1.object_node.symbol_type.name=="("):
+                if(len(node1.node_list)==0 and str(type(node1.object_node)) == "<class 'objects.Token.Token'>" and node1.object_node.symbol_type.name=="("):
                     if(inner_counter!=0 and inner_counter!=1):
                         if(self.node_list[inner_counter-1].object_node.symbol_type.name == 'id' and 
                             (self.node_list[inner_counter-2].object_node.symbol_type.name == 'void' or self.node_list[inner_counter-2].object_node.symbol_type.name == 'type')):
                             print("push scope")
                             symbol_table.append([])
 
-                if(len(node1.node_list)==0 and node1.object_node.symbol_type.name=="{"):
+                if(len(node1.node_list)==0 and str(type(node1.object_node)) == "<class 'objects.Token.Token'>" and node1.object_node.symbol_type.name=="{"):
                     print("push scope")
                     symbol_table.append([])
 
-                if(len(node1.node_list)==0 and node1.object_node.symbol_type.name=="}"):
+                if(len(node1.node_list)==0 and str(type(node1.object_node)) == "<class 'objects.Token.Token'>" and node1.object_node.symbol_type.name=="}"):
                     print("pop scope")
                     #del main_program.symbol_table[-1]
 
