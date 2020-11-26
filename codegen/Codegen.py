69de5b9c6e5801177d941b23549fb6e78f931b81
andresryes <enriquebolanos@ufm.edu>
2020-11-25 20:49:41 -0600

    codgen -1.0

--- codegen/Codegen.py
+++ codegen/Codegen.py
@@ -10,9 +10,11 @@ from anytree.exporter import DotExporter
 
 class Codegen:
     def codegen(self, main_program, debug):
-        program_ui = main_program.program_tree
-        for pre, fill, node in RenderTree(program_ui):
-            print("%s%s" % (pre, node.name))
+        # program_ui = main_program.program_tree
+        # for pre, fill, node in RenderTree(program_ui):
+        #     print("%s%s" % (pre, node.name))
         print("debug", debug)
         for node_irt in main_program.irt_list:
             print(node_irt.instruction)
+
+        
